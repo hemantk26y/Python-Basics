150feebc70daedc1e19cbef88c2579d6ef3464fb
Hemant Kumar <145690488+hemantk26y@users.noreply.github.com>
2024-03-25 23:21:52 +0530

    Update A number is prime or not.py

--- A number is prime or not.py
+++ A number is prime or not.py
@@ -1,12 +1,12 @@
-""" WAP to check whether a number is prime or not """
+# WAP to check whether a number is prime or not
 
-num = int(input("Enter the number :"))
-c = 0
-for i in range(1, num + 1):
-    if num % i == 0:
-        c += 1
+number = int(input("Enter the number to check :"))
+count = 0
+for num in range(1, number + 1):
+    if number % num == 0:
+        count += 1
 
-if c == 2:
+if count == 2:
     print(num,"is a prime number")
 else:
     print(num, "is not a prime number")
